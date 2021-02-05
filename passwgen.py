bc69087f6068892ac5161459512323f2f8d1c31e
Klemen Vovk <klemen.vovk3@gmail.com>
2021-02-05 23:04:22 +0000

    Implemented a new algorithm for generating which ensures that there is no space at the ends, no consecutive spaces and every selected group is included in the output.

--- passwgen.py
+++ passwgen.py
@@ -1,29 +1,66 @@
-from random import choices
+from random import choice, randint, shuffle
+from itertools import groupby
+
 
 class PasswordGenerator():
     # Lists of releveant indexes in the ASCII table.
-    LOWERCASE_IXS = list(range(97,122 + 1)) 
-    UPPERCASE_IXS = list(range(65,90 + 1))
-    NUMBERS_IXS = list(range(48,57 + 1))
-    SPACE_IX = [32]
-    SYMBOLS_IXS = list(range(33,47 + 1)) + list(range(58,64 + 1)) + list(range(91,96 + 1)) + list(range(123, 126 + 1))
+    LOWERCASE_IXS = list(range(97, 122 + 1))
+    UPPERCASE_IXS = list(range(65, 90 + 1))
+    NUMBERS_IXS = list(range(48, 57 + 1))
+    SPACE_IX = 32
+    SYMBOLS_IXS = list(range(33, 47 + 1)) + list(range(58, 64 + 1)) + \
+        list(range(91, 96 + 1)) + list(range(123, 126 + 1))
 
     # Depending on the given options, constructs a union of relevant indexes from above.
     def generate_relevant_ixs(self, options):
         population = []
-        if options["hasLowercase"]: population += self.LOWERCASE_IXS
-        if options["hasUppercase"]: population += self.UPPERCASE_IXS
-        if options["hasNumbers"]: population += self.NUMBERS_IXS
-        if options["hasSpaces"]: population += self.SPACE_IX
-        if options["hasSymbols"]: population += self.SYMBOLS_IXS
+        if options["hasLowercase"]:
+            population += self.LOWERCASE_IXS
+        if options["hasUppercase"]:
+            population += self.UPPERCASE_IXS
+        if options["hasNumbers"]:
+            population += self.NUMBERS_IXS
+        if options["hasSpaces"]:
+            population += self.SPACE_IX
+        if options["hasSymbols"]:
+            population += self.SYMBOLS_IXS
         return population
 
-    # FIXME Prevent spaces on the ends.
-    # FIXME Make sure all chosen options are actually included if possible.
+    def strip(self, passwd):
+        while(passwd[0] == self.SPACE_IX or passwd[-1] == self.SPACE_IX):
+            if(passwd[0] == self.SPACE_IX):
+                del passwd[0]
+            if(passwd[-1] == self.SPACE_IX):
+                del passwd[-1]
+        return passwd
+
     def generate_password(self, length, options):
-        population = self.generate_relevant_ixs(options)
-        chosen_ixs = choices(population=population, k=length)
-        return "".join([chr(ix) for ix in chosen_ixs])
+        passwd = []
+        # Generates random characters until given length is reached
+        while len(passwd) < length:
+            # Uses round-robin for generating characters from specific chosen groups. This ensures that the result contains all selected groups if the length is sufficient.
+            if options["hasLowercase"] and len(passwd) < length:
+                passwd.append((choice(seq=self.LOWERCASE_IXS)))
+            if options["hasUppercase"] and len(passwd) < length:
+                passwd.append((choice(seq=self.UPPERCASE_IXS)))
+            if options["hasNumbers"] and len(passwd) < length:
+                passwd.append((choice(seq=self.NUMBERS_IXS)))
+            if options["hasSpaces"] and len(passwd) < length:
+                passwd.append(self.SPACE_IX)
+            if options["hasSymbols"] and len(passwd) < length:
+                passwd.append((choice(seq=self.SYMBOLS_IXS)))
+
+            # Shuffles the password, removes front and end spaces and removes consecutive spaces.
+            shuffle(passwd)
+            passwd = self.strip(passwd)
+            passwd = [x for x, y in groupby(passwd) if len(list(y)) < 2]
+
+            # Ensures atleast 1 space is in the password(removing adjacent spaces might have removed all spaces).
+            if(options["hasSpaces"] and passwd.count(32) == 0):
+                passwd.insert(randint(1, len(passwd) - 1), 32)
+
+        return "".join([chr(ix) for ix in passwd])
+
 
 options = {
     "hasLowercase": True,
